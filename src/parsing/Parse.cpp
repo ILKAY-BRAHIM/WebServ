c4074e0a21d35c404a098de44f7259ff8b04b155
Adnane Mansar <admansar@c1r7p1.1337.ma>
2023-12-18 11:58:52 +0100

    cgi_added

--- src/parsing/Parse.cpp
+++ src/parsing/Parse.cpp
@@ -253,6 +253,8 @@ void Parse::fill_locations(vectstr_t vector, int &i, t_location &location)
 		fill_parts(vector, i, location.alias, "alias : ");
 	else if (vector[i] == "index")
 		fill_indexs(vector, i, location.index, "index : ");
+	else if (vector[i] == "cgi_index")
+		fill_indexs(vector, i, location.cgi_index, "cgi_index : ");
 	else if (vector[i] == "proxy_pass")
 		fill_parts(vector, i, location.proxy_pass, "proxy_pass : ");
 	else if (vector[i] == "rewrite")
