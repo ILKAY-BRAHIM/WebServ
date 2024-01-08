2be16fe11347c3f4f3f84422deac41b03ff0ee80
Brahim Chifour <bchifour@c1r7p2.1337.ma>
2024-01-08 17:42:24 +0100

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -2,70 +2,6 @@
 #include "Server_.hpp"
 #include "response.hpp"
 
-void	printServer(std::vector<t_server > serv)
-{
-	for(size_t i = 0, j = 0; i < serv.size(); i++)
-	{
-		std::cout << "server " << i << std::endl;
-		std::cout << "	name: " << serv[i].name << std::endl;
-		std::cout << "	host: " << serv[i].host << std::endl;
-		std::cout << "	root: " << serv[i].root << std::endl;
-		std::cout << "	port: ";
-		for (j = 0; j < serv[i].port.size(); j++)
-			std::cout << serv[i].port[j] << " ";
-		std::cout << std::endl;
-		std::cout << "	index: ";
-		for (j = 0; j < serv[i].index.size(); j++)
-			std::cout << serv[i].index[j] << " ";
-		std::cout << std::endl;
-		{
-			std::cout << "	error_page: ";
-			size_t h = 0;
-			while (h < serv[i].error_page.first.size())
-			std::cout << serv[i].error_page.first[h++] << " ";
-			std::cout << serv[i].error_page.second << std::endl;
-		}
-		std::cout << "	redirect: " << serv[i].redirect << std::endl;
-		std::cout << "	timeout: " << serv[i].timeout << std::endl;
-		std::cout << "	allow_methods: ";
-		for (j = 0; j < serv[i].allow_methods.size(); j++)
-			std::cout << serv[i].allow_methods[j] << " ";
-		std::cout << std::endl;
-		std::cout << "	location: " << std::endl;
-		for (j = 0; j < serv[i].locations.size(); j++)
-		{
-			std::cout << "	location " << j << std::endl;
-			std::cout << "		path: " << serv[i].locations[j].path << std::endl;
-			std::cout << "		root: " << serv[i].locations[j].root << std::endl;
-			std::cout << "		alias: " << serv[i].locations[j].alias << std::endl;
-			std::cout << "		index: ";
-			for (size_t k = 0; k < serv[i].locations[j].index.size(); k++)
-				std::cout << serv[i].locations[j].index[k] << " ";
-			std::cout << std::endl;
-			std::cout << "		alllow_methods: ";
-			for (size_t k = 0; k < serv[i].locations[j].allow_methods.size(); k++)
-				std::cout << serv[i].locations[j].allow_methods[k] << " ";
-			std::cout << std::endl;
-			std::cout << "		cgi_index: ";
-			for (size_t k = 0; k < serv[i].locations[j].cgi_index.size(); k++)
-				std::cout << serv[i].locations[j].cgi_index[k] << " ";
-			std::cout << std::endl;
-			std::cout << "		redirect: " << serv[i].locations[j].redirect << std::endl;
-			if (serv[i].locations[j].internal == true)
-				std::cout << "		internal: on" << std::endl;
-			else
-				std::cout << "		internal: off" << std::endl;
-			if (serv[i].locations[j].autoindex == true)
-				std::cout << "		autoindex: on" << std::endl;
-			else
-				std::cout << "		autoindex: off" << std::endl;
-			if (serv[i].locations[j].client_body_size.size())
-				std::cout << "		client_body_size: " << serv[i].locations[j].client_body_size << std::endl;
-		}
-		std::cout << std::endl;
-	}
-}
-
 int main (int ac, char **av, char **env)
 {
 	std::vector<t_server> servers;
@@ -76,12 +12,6 @@ int main (int ac, char **av, char **env)
 		return (1);
 	
 	Response resp(servers, env);
-	// std::string h("hello");
-	// Message *mes = resp.generateResponse(h);
-	// std::cout << mes->getResponse() << std::endl;
-	//continue above this line
-	// printServer(servers);
-	// pause();
 	try{
 
 		Server serv(servers, resp);
@@ -93,6 +23,5 @@ int main (int ac, char **av, char **env)
 		std::cerr << "\nErrorr: " << e << std::endl;
 		return (1);
 	}
-
-	// get_request(servers);
+	return (0);
 }
