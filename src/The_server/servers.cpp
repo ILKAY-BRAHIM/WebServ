33ff8bcf82ccc24d920c3563105352b79b1b1fd7
Brahim Chifour <bchifour@c1r7p2.1337.ma>
2024-01-08 17:31:41 +0100

    finish

--- src/The_server/servers.cpp
+++ src/The_server/servers.cpp
@@ -2,32 +2,16 @@
 
 Servers::Servers(std::string request)
 {
-    // this->request = request;
     (void)request;
     request.clear();
     this->total = 0;
     partis = 0;
     bypass = 0;
     pass_total = 0;
-    // std::cout << "request: " << this->request << std::endl;
 }
-
-// Servers::Server(const Responce &responce)
-// {
-//     this->responce = responce;
-// }
 
 void Servers::collect_req(char* request, int size)
 {
-    // this->request += request;
     this->request.append(request, size);
-    // std::cout << "request: " << this->request << std::endl;
     return;
 }
-
-
-// void Servers::set_responce(Message *responce)
-// {
-//     this->responce = responce;
-//     return;
-// }
